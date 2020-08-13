d4db43ac0d7779c9619625909d846e9c5ba5c804
Lurker <wangzhecheng@yeah.net>
2020-08-14 00:27:15 +0800

    2020-08-14_00:27:15

--- update.sh
+++ update.sh
@@ -1,13 +1,9 @@
 cp ~/.bash_profile .
 cp ~/.zshrc .
-cp ~/.vimrc .
-mkdir .vim
-cp ~/.vim/coc-settings.json .vim/coc-settings.json
-cp ~/.vim/tasks.ini .vim/tasks.ini
+cp ~/.vim .
 mkdir .config
-mkdir .config/coc
-cp -r ~/.config/alacritty .config/alacritty
-cp -r ~/.config/coc/ultisnips .config/coc/ultisnips
+cp -r ~/.config/alacritty .config/
+cp -r ~/.config/coc/ultisnips .config/coc/
 
 git add .
 git commit -am $(date "+%Y-%m-%d_%H:%M:%S")
